1b12c8cb6533ff9528e3dbf6f2a85c11623d8ebe
SaiTeja <madhavasaiteja@gmail.com>
2025-12-03 13:51:36 +0530

    day 1 part 1 added

--- 2025/day_1.cpp
+++ 2025/day_1.cpp
@@ -2,6 +2,22 @@
 using namespace std; 
 
 void Sol() {
+	// part - 1 
+	string s;
+    int cnt = 0, dial = 50, tot = 100;
+
+    while (cin >> s) {
+        int dir = (s[0] == 'L') ? +1 : -1;
+        int num = 0;
+        for (int i = 1; i < s.size(); i++) {
+            num = (num * 10) + (s[i] - '0');
+        }
+        dial += (dir * num);
+        dial %= tot;
+        cnt += (not dial);
+    }
+
+    cout << cnt << endl;
 
 }
 
@@ -11,3 +27,4 @@ int main() {
 
 	return 0;
 }
+
